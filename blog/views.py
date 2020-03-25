5cade72c033cf3b8f0d53dbfd450542aa5477bf8
Soif Akhter <soifakhter@gmail.com>
2020-03-25 15:32:06 +0530

    Template Used

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(request):
-	return render(request,'templates/blog/home.html')
+	return render(request,'blog/home.html')
 
 def about(request):
-	return HttpResponse('<h1>About Blog<h1>')
+	return render(request,'blog/about.html')
 
 	
