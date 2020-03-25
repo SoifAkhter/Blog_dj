21532bd6962b52dfd5b36b82206ab3791233683c
Soif Akhter <soifakhter@gmail.com>
2020-03-25 16:06:40 +0530

    Added Post

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+	{
+	'author' : 'Soif',
+	'title' : 'Blog 1',
+	'content' : "First Blog post",
+	'date' : "25th Mar, 2020" 
+	},
+	{
+	'author' : 'Samar',
+	'title' : 'Blog 2',
+	'content' : "second Blog post",
+	'date' : "26th Mar, 2020"
+	}
+]
 
 def home(request):
-	return render(request,'blog/home.html')
+	cont = {
+		'posts' : posts
+	}
+
+	return render(request,'blog/home.html', cont)
 
 def about(request):
-	return render(request,'blog/about.html')
+	return render(request,'blog/about.html', {'title': "About"})
 
 	
